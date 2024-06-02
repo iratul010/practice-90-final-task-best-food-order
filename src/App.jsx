5d366d9ad45106963abeff098da41316b164e9f1
Ratul Islam <iratul010@gmail.com>
2024-06-02 22:39:08 +0600

    initial-state

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+const App = () => {
+  return (
+    <div>
+     <h2 className="text-2xl"> content</h2>
+     <button className="btn btn-primary">Primary</button>
+     <button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-ghost">Ghost</button>
+<button className="btn btn-link">Link</button>
+    </div>
+  );
+};
+
+export default App;
